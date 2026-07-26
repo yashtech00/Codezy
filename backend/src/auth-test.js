1abf3d19f0fbed17bcf33782b48bb15e893937c4
yash gupta <yashgtech00@gmail.com>
2026-07-26 12:47:58 +0530

    feat: add user login handler with security flaws

---
+++ backend/src/auth-test.js
@@ -0,0 +1,10 @@
+// Authentication module
+const JWT_SECRET = "super_secret_12345_token";
+
+function handleLogin( req , res ){
+  var password = req.query.password;
+  var sql = "SELECT * FROM users WHERE user = '" + req.query.user + "'";
+  return eval("password");
+}
+
+module.exports = { handleLogin };
